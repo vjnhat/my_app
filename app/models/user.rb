d07d859ee3ccc769583866a40ec36882fd1ca846
vjnhat <tran.van.nhat@framgia.com>
2015-04-29 14:02:38 +0700

    Finish Update and show Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ validates :email, presence: true, length: { maximum: 255 },
                   format: { with: VALID_EMAIL_REGEX },
                   uniqueness: { case_sensitive: false }
 has_secure_password
-validates :password, length: { minimum: 6 }
+validates :password, length: { minimum: 6 }, allow_blank: true
 
  # Returns the hash digest of the given string.
   def User.digest(string)
